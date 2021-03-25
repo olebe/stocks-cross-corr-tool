fa52c0365f2aa671e29495ee9d76bd182882a3f3
Ole <ole-be@gmx.de>
2021-03-25 16:35:32 +0100

    coinbase_api with account data and bollinger bands example

--- coinbase_api.py
+++ coinbase_api.py
@@ -1,11 +1,21 @@
 import cbpro
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-
+#public client
 public_client = cbpro.PublicClient()
 
+#authenticate private client
+auth= pd.read_csv('auth.txt',header=None)
+key = auth.iloc[0][0]
+secret = auth.iloc[1][0]
+passphrase = auth.iloc[2][0]
+auth_client = cbpro.AuthenticatedClient(key, secret, passphrase)
+
+
+
+##  public client functions
 
 def historic_price (product, start= None, end= None, granularity=300):
     #product example: 'REN-USD'
@@ -13,16 +23,65 @@ def historic_price (product, start= None, end= None, granularity=300):
     historic= pd.DataFrame(public_client.get_product_historic_rates(product,start=None, end=None,granularity= granularity))
     historic[0]= pd.to_datetime(historic[0], unit='s')
     historic= historic.rename(columns={0: "time", 1: "low",2: "high", 3: "open",4: "close", 5: "volume"})
+    historic=historic.sort_values ('time')
     return historic
 
+def bollinger (df, window=15, no_of_std=2 ):
+    #Set number of days (window) and standard deviations (no_of_std) to use for rolling lookback period for Bollinger band calculation
 
-price_hist= historic_price ('BTC-EUR', granularity = 60)
+    #Calculate rolling mean and standard deviation using number of days set above
+    rolling_mean = df['close'].rolling(window).mean()
+    rolling_std = df['close'].rolling(window,min_periods=window).std()
+
+    #create two new DataFrame columns to hold values of upper and lower Bollinger bands
+    
+    df['Rolling Mean'] = rolling_mean
+    df['Bollinger High'] = rolling_mean + (rolling_std * no_of_std)
+    df['Bollinger Low'] = rolling_mean - (rolling_std * no_of_std)
+    df= df.set_index('time')
+    
+    return df
+
+
+## private client functions
+
+def show_account():
+    account = pd.DataFrame(auth_client.get_accounts())
+    account_num = account
+    account_num['balance'] = pd.to_numeric(account.balance, downcast = 'float')
+    account_num['available'] = pd.to_numeric(account.available, downcast = 'float')
+    account_num['hold'] = pd.to_numeric(account.hold, downcast = 'float')
+    return account_num
+
+def get_current_account_prices(accounts):
+    accounts['Current_price_in_EUR']=0
+
+    for i in range(len(accounts)):
+
+        #print (accounts['currency'].iloc[i])
+        temp_currency=accounts['currency'].iloc[i]
+
+        try:
+            temp_price = pd.to_numeric(public_client.get_product_ticker(product_id=temp_currency+'-EUR')['bid'], downcast = 'float')
+            accounts['Current_price_in_EUR'].iloc[i]=temp_price
+
+        except: accounts['Current_price_in_EUR'].iloc[i]=np.nan
+            
+    return accounts
 
 
 
-plt.plot(price_hist.time, price_hist.close)
-plt.ylabel('€/BTC')
-#plt.plot(price_hist.time, price_hist.open)
-#plt.bar(eth_hist.time, eth_hist.volume)
+print('Which coins do I own?')
 
-plt.xticks(rotation=45)
+accounts = show_account()
+accounts_notzero = accounts[accounts['balance']!=0].reset_index()
+accounts_with_prices = get_current_account_prices(accounts_notzero)
+print(accounts_with_prices.to_string())
+
+
+print('\n\n\n\nShow Bollinger Bands for EOS-EUR')
+
+
+price_hist= historic_price ('BTC-EUR', granularity = 21600)
+price = bollinger(price_hist,20,2)
+price[['close','Bollinger High','Bollinger Low','Rolling Mean']].plot(figsize= (20,10))
